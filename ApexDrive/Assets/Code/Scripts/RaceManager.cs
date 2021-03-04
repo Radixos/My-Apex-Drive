cabdeb1cededbbdb2ffaf3c02514bcd60f82d6a6
Mani <manikkanaga@gmail.com>
2021-03-04 20:25:32 +0000

    Implemented race position logic

---
+++ ApexDrive/Assets/Code/Scripts/RaceManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RaceManager : MonoBehaviour
+{
+
+    public List<PositionUpdate> raceCars;
+
+    public int totalColliders;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        for(int i = 0; i < raceCars.Count; i++)
+        {
+            for (int j = 0; j < raceCars.Count; j++)
+            {
+                if(raceCars[i].gameObject.GetInstanceID() != 
+                    raceCars[j].gameObject.GetInstanceID())
+                {
+                    //if(raceCars[i].laps > raceCars[j].laps)
+                    //{
+
+                    //}
+                    
+                    if(raceCars[i].collidersHit > raceCars[j].collidersHit)
+                    {
+
+                        if (raceCars.IndexOf(raceCars[i]) > raceCars.IndexOf(raceCars[j]))
+                        {
+                            PositionUpdate temp = raceCars[j];
+                            raceCars[j] = raceCars[i];
+                            raceCars[i] = temp;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ ApexDrive/Assets/Code/Scripts/TestDrive.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDrive : MonoBehaviour
+{
+
+    public bool isAI;
+
+    private float AIMoveSpeed;
+    private float playerMoveSpeed;
+
+    private Rigidbody myRigidbody;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AIMoveSpeed = 3.0f;
+        playerMoveSpeed = 7.0f;
+
+        myRigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        myRigidbody.velocity = Vector3.zero;
+
+        if(isAI)
+        {
+            myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, myRigidbody.velocity.y, AIMoveSpeed);
+        }
+        else
+        {
+            if(Input.GetKey(KeyCode.W))
+            {
+                myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, myRigidbody.velocity.y, playerMoveSpeed);
+            }
+            else if(Input.GetKey(KeyCode.S))
+            {
+                myRigidbody.velocity = new Vector3(myRigidbody.velocity.x, myRigidbody.velocity.y, -playerMoveSpeed);
+            }
+        }
+    }
+}
