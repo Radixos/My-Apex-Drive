fe999d312e673f6769eef6af1e8dd14cc07c124b
BigTimeJason <49235257+BigTimeJason@users.noreply.github.com>
2021-03-08 15:10:23 +0000

    better car physics
    drifting with left shift also i messed up and lost some progress but i think its ok

--- ApexDrive/Assets/Code/Scripts/SphereCarController.cs
+++ ApexDrive/Assets/Code/Scripts/SphereCarController.cs
@@ -5,9 +5,18 @@ using UnityEngine;
 public class SphereCarController : MonoBehaviour
 {
     public Rigidbody sphereCollider;
+    public Transform carModel;
 
     private float horizontal;
     private float vertical;
+
+    [Header("Drifting Options")]
+    [SerializeField]
+    private bool isDrifting;
+    [SerializeField]
+    [Range(0, 1)]
+    [Tooltip("The higher this is, the faster the car will need to be going before you can initiate a drift")]
+    private float driftSpeedThresholdPercent;
 
     [Header("Turning Options")]
     [SerializeField]
@@ -21,22 +30,19 @@ public class SphereCarController : MonoBehaviour
 
     [Header("Acceleration Options")]
     [SerializeField]
-    private float accelerationRate;
+    private float maxAcceleration;
     [SerializeField]
     private float currAcceleration;
     [SerializeField]
-    private float maxAcceleration;
+    [Range(0, 1)]
+    [Tooltip("The lower this is, the faster the car will go")]
+    private float friction;
 
     [Header("Speed Options")]
     [SerializeField]
-    private float currSpeed;
-    [SerializeField]
     private float maxSpeed;
-
     [SerializeField]
-    [Tooltip("Speed * Friction. A value of 0 means the car will not slow down.")]
-    [Range(0.1f, 1f)]
-    private float friction;
+    private float currSpeed;
 
     private void Update()
     {
@@ -47,10 +53,29 @@ public class SphereCarController : MonoBehaviour
     void FixedUpdate()
     {
         //Follow Collider
-        transform.position = sphereCollider.position;
+        carModel.position = sphereCollider.position;
 
         HandleMovement();
         HandleSteering();
+        HandleAnimation();
+
+        if (Input.GetKey(KeyCode.LeftShift) && currSpeed / maxSpeed > driftSpeedThresholdPercent && horizontal != 0)
+        {
+            isDrifting = true;
+        } else
+        {
+            isDrifting = false;
+        }
+    }
+
+    void HandleAnimation()
+    {
+        //Raycast down - angle model based on normal of floor
+
+        RaycastHit hit;
+        Physics.Raycast(carModel.position, -carModel.up, out hit, 1f);
+
+        //Turn wheels/steering wheel based on horizontal input
     }
 
     void HandleSteering()
@@ -59,34 +84,44 @@ public class SphereCarController : MonoBehaviour
         {
             return;
         }
+
         float targetAngle = currAngle + (horizontal * maxTurnAngle);
-        float angle = Mathf.SmoothDamp(transform.localEulerAngles.y, targetAngle, ref turnVelocity, turnSpeed);
-        transform.localEulerAngles = new Vector3(0, angle, 0);
-        currAngle = transform.localEulerAngles.y;
+        float angle = Mathf.SmoothDamp(carModel.localEulerAngles.y, targetAngle, ref turnVelocity, turnSpeed);
+        if (isDrifting)
+        {
+            maxTurnAngle = 60;
+        } else
+        {
+            maxTurnAngle = 25;
+        }
+        carModel.localEulerAngles = new Vector3(0, angle, 0);
+        currAngle = carModel.localEulerAngles.y;
     }
 
     void HandleMovement()
     {
-        if (vertical != 0)
-        {
-            currAcceleration += vertical * accelerationRate;
-        }
+        currAcceleration = vertical * maxAcceleration;
+        currSpeed = sphereCollider.velocity.magnitude;
 
-        //Forward Acceleration
-        sphereCollider.AddForce(transform.forward * currAcceleration);
+        //Acceleration only if not too fast
+        if (currSpeed < maxSpeed)
+        {
+            sphereCollider.AddForce(carModel.forward * currAcceleration);
+        }
 
         //Clamp Acceleration
         if(Mathf.Abs(currAcceleration) > maxAcceleration)
         {
-            currAcceleration = maxAcceleration * vertical;
+            currAcceleration = maxAcceleration;
         }
 
-        currSpeed = sphereCollider.velocity.magnitude;
-
-        //Clamp Speed
-        if(currSpeed >= maxSpeed)
+        //Lower speed while turning and not drifting
+        if (!isDrifting && horizontal != 0)
         {
-            sphereCollider.AddForce(transform.forward * -1 * currAcceleration);
+            sphereCollider.AddForce(-carModel.forward * currSpeed * friction);
         }
+
+        //Reduce Speed
+        //sphereCollider.AddForce(-carModel.forward * currSpeed * friction);
     }
 }
