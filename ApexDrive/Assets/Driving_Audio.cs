7555e3620a02d1696e7dab58c6a0959c4961483f
mudkipz5 <mudkipz5@yahoo.co.uk>
2021-04-21 11:51:07 +0100

    Added driving sounds! And ambience!
    Driving sounds - rough
    Acceleration and deceleration are there but not perfect
    Helps teach me good ways to implement it, however.
    Currently it's not good, but once I learn how to read velocity values and we have a deeper controller it should be better

---
+++ ApexDrive/Assets/Driving_Audio.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Driving_Audio : MonoBehaviour
+{
+    FMOD.Studio.EventInstance Engine;
+    FMOD.Studio.EventDescription SPD;
+    FMOD.Studio.PARAMETER_DESCRIPTION Speed;
+    FMOD.Studio.PARAMETER_ID RPM;
+
+    FMOD.Studio.PARAMETER_DESCRIPTION Accelleration;
+    FMOD.Studio.PARAMETER_ID ACE;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Engine = FMODUnity.RuntimeManager.CreateInstance("event:/TukTuk/Engine");
+
+        SPD = FMODUnity.RuntimeManager.GetEventDescription("event:/TukTuk/Engine");
+        SPD.getParameterDescriptionByName("RPM", out Speed);
+        RPM = Speed.id;
+
+        SPD.getParameterDescriptionByName("Accelleration", out Accelleration);
+        ACE = Accelleration.id;
+
+        Engine.start();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FMODUnity.RuntimeManager.AttachInstanceToGameObject(Engine, GetComponent<Transform>(), GetComponent <Rigidbody>());
+        if(Input.GetMouseButtonDown(1))
+        {
+            Engine.setParameterByID(RPM, 1f);
+            Engine.setParameterByID(ACE, 1f);
+        }
+        else if(Input.GetMouseButtonUp(1))
+        {
+            Engine.setParameterByID(RPM, 0f);
+            Engine.setParameterByID(ACE, 0f);
+        }
+    }
+}
