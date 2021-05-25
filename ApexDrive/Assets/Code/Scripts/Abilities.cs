8a24d6e3cc85467d5ed531f0d89ea53837c14748
mudkipz5 <mudkipz5@yahoo.co.uk>
2021-05-25 21:23:00 +0100

    Added audio to Cars

--- ApexDrive/Assets/Code/Scripts/Abilities.cs
+++ ApexDrive/Assets/Code/Scripts/Abilities.cs
@@ -26,6 +26,9 @@ public class Abilities : MonoBehaviour
     [SerializeField]
     private float rampageTimer;
 
+    //FMOD Stuff
+    FMOD.Studio.EventInstance Ability;
+
     private void Start()
     {
         carInputHandler = GetComponent<CarInputHandler>();
