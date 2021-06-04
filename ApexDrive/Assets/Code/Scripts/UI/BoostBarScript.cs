6d5c288a581546e7db43a7fa8532e0d72ae5692f
Zach Horton <Zachary2.Horton@live.uwe.ac.uk>
2021-06-04 07:49:58 +0100

    UI Bars Progress
    Basic UI bars have been added. A prefab canvas has been created holding four bars, each with a custom made border and inner bar (which has been given a slider property as to fit appropriately). Currently they also hold text objects, but these are to be implemented further once a solid ranking can be accessed. Through a new Bar script, these objects as well as a gradient are declared and allocated. With the slider manipulation, the poweramount from the Abilities script in each vehicle is used and alongside this, also affects the slider's colour due to the aforementioned use of a Gradient. However, this only currently shows one colour at a time going from red to orange to yellow, and a bar with the gradient flowing through it is more desired. This is being heavily looked into, and should be implemented soon alongside proper ranking portrayal.

---
+++ ApexDrive/Assets/Code/Scripts/UI/BoostBarScript.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class BoostBarScript : MonoBehaviour
+{
+    private Canvas UICanvas;
+    [SerializeField] private RaceManager vehicleManager;
+    private GameObject[] boostBarObjects;
+
+    private Slider[] boostBarSliders;
+    private Image[] boostBarImages;
+    private TextMeshProUGUI[] boostBarText;
+    private Gradient barGradient;
+    public Gradient testGradient;
+    private float colourTimer, alphaTimer;
+
+    GradientColorKey[] colourKeys;
+    GradientAlphaKey[] alphaKeys;
+
+    void Start()
+    {
+        //set gradients
+        //testDriveScript[i].powerAmount
+        UICanvas = GetComponent<Canvas>();
+        int vehicleNum = vehicleManager.raceCars.Count;
+        boostBarObjects = new GameObject[vehicleNum];
+        boostBarSliders = new Slider[vehicleNum];
+        boostBarImages = new Image[vehicleNum];
+        boostBarText = new TextMeshProUGUI[vehicleNum];
+
+        for (int i = 0; i < vehicleNum; i++)
+        {
+            boostBarObjects[i] = UICanvas.transform.GetChild(i).gameObject;
+            boostBarObjects[i].SetActive(true);
+            boostBarSliders[i] = boostBarObjects[i].GetComponent<Slider>();
+            boostBarImages[i] = boostBarObjects[i].transform.GetChild(0).GetComponent<Image>();
+            boostBarText[i] = boostBarObjects[i].transform.GetChild(2).GetComponent<TextMeshProUGUI>();
+        }
+
+        InitGradient();
+    }
+
+    private void InitGradient()
+    {
+        barGradient = new Gradient();
+        alphaKeys = new GradientAlphaKey[3];
+        alphaTimer = 0.0f;
+
+        for (int j = 0; j < alphaKeys.Length; j++)
+        {
+
+            alphaKeys[j].alpha = 1.0f;
+            alphaKeys[j].time = alphaTimer;
+            alphaTimer += 0.5f;
+        }
+
+        colourKeys = new GradientColorKey[3];
+        colourKeys[0].color = Color.yellow;
+        colourKeys[1].color = new Color(1.0f, 0.55f, 0.0f);
+        colourKeys[2].color = Color.red;
+        colourTimer = 0.0f;
+
+        for (int k = 0; k < colourKeys.Length; k++)
+        {
+            colourKeys[k].time = colourTimer;
+            colourTimer += 0.5f;
+        }
+        barGradient.SetKeys(colourKeys, alphaKeys);
+        
+    }
+
+    void Update()
+    {
+        SetSliders();
+        SetText();
+
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            vehicleManager.raceCars[0].gameObject.GetComponent<AbilityCollision>().carAbilities.powerAmount -= 0.15f * Time.deltaTime;
+        }
+    }
+    void SetSliders()
+    {
+        for (int i = 0; i < vehicleManager.raceCars.Count; i++)
+        {
+            PositionUpdate currentVehicle = vehicleManager.raceCars[i];
+            AbilityCollision abilities = currentVehicle.gameObject.GetComponent<AbilityCollision>();
+            boostBarSliders[i].value = abilities.carAbilities.powerAmount;
+            boostBarImages[i].color = barGradient.Evaluate(boostBarSliders[i].value);
+        }
+    }
+    void SetText()
+    {
+        for (int i = 0; i < vehicleManager.raceCars.Count; i++)
+        {
+            PositionUpdate currentvehicle = vehicleManager.raceCars[i];
+        }
+    }
+}
