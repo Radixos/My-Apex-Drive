f75b9200919c5ffafdddce45324590d48ed24e46
Zach Horton <Zachary2.Horton@live.uwe.ac.uk>
2021-06-06 07:14:33 +0100

    Cleanup of unneeded code and bar colour method
    As a whole, a uniform colour bar has been decided by the group as the most fitting for the project, so work on implementing a visual gradient esque bar has been dropped. To add, Color.Lerp results in the same outcome as the lengthy setup and use of a gradient, so this has also been replaced as to be more proficient and clean up code.

--- ApexDrive/Assets/Code/Scripts/UI/BoostBarScript.cs
+++ ApexDrive/Assets/Code/Scripts/UI/BoostBarScript.cs
@@ -6,31 +6,31 @@ using TMPro;
 
 public class BoostBarScript : MonoBehaviour
 {
-    private Canvas UICanvas;
+    private Canvas UICanvas; //overarching use variables
     [SerializeField] private RaceManager vehicleManager;
     private int vehicleNum;
 
-    private GameObject[] boostBarObjects;
-    private Slider[] boostBarSliders;
+    private GameObject[] boostBarObjects; 
+    private Slider[] boostBarSliders; //object children
     private TextMeshProUGUI[] boostBarText;
     private Image[] boostBarImages;
 
-    private Gradient barGradient;
-    private float colourTimer, alphaTimer;
-    GradientColorKey[] colourKeys;
-    GradientAlphaKey[] alphaKeys;
+    //private Gradient barGradient; //gradient related properties
+    //private float colourTimer, alphaTimer;
+    //GradientColorKey[] colourKeys;
+    //GradientAlphaKey[] alphaKeys;
 
     void Start()
     {
         UICanvas = GetComponent<Canvas>();
         vehicleNum = vehicleManager.raceCars.Count;
         InitBarObjects();
-        InitGradient();
+        //InitGradient();
     }
 
     private void InitBarObjects()
     {
-        boostBarObjects = new GameObject[vehicleNum];
+        boostBarObjects = new GameObject[vehicleNum]; //initialise objects and their children
         boostBarSliders = new Slider[vehicleNum];
         boostBarText = new TextMeshProUGUI[vehicleNum];
         boostBarImages = new Image[vehicleNum];
@@ -45,43 +45,37 @@ public class BoostBarScript : MonoBehaviour
         }
     }
 
-    private void InitGradient()
-    {
-        barGradient = new Gradient();
-        alphaKeys = new GradientAlphaKey[3];
-        alphaTimer = 0.0f;
+    //private void InitGradient()
+    //{
+    //    barGradient = new Gradient();
+    //    alphaKeys = new GradientAlphaKey[3];
+    //    alphaTimer = 0.0f;
 
-        for (int j = 0; j < alphaKeys.Length; j++)
-        {
-            alphaKeys[j].alpha = 1.0f;
-            alphaKeys[j].time = alphaTimer;
-            alphaTimer += 0.5f;
-        }
+    //    for (int j = 0; j < alphaKeys.Length; j++)
+    //    {
+    //        alphaKeys[j].alpha = 1.0f;
+    //        alphaKeys[j].time = alphaTimer;
+    //        alphaTimer += 0.5f;
+    //    }
 
-        colourKeys = new GradientColorKey[3];
-        colourKeys[0].color = Color.yellow;
-        colourKeys[1].color = new Color(1.0f, 0.55f, 0.0f);
-        colourKeys[2].color = Color.red;
-        colourTimer = 0.0f;
+    //    colourKeys = new GradientColorKey[3];
+    //    colourKeys[0].color = Color.yellow;
+    //    colourKeys[1].color = new Color(1.0f, 0.55f, 0.0f);
+    //    colourKeys[2].color = Color.red;
+    //    colourTimer = 0.0f;
 
-        for (int k = 0; k < colourKeys.Length; k++)
-        {
-            colourKeys[k].time = colourTimer;
-            colourTimer += 0.5f;
-        }
-        barGradient.SetKeys(colourKeys, alphaKeys);
-    }
+    //    for (int k = 0; k < colourKeys.Length; k++)
+    //    {
+    //        colourKeys[k].time = colourTimer;
+    //        colourTimer += 0.5f;
+    //    }
+    //    barGradient.SetKeys(colourKeys, alphaKeys);
+    //}
 
     void Update()
     {
         SetSliders();
-        //DeterminePositions();
         ApplyText();
-
-        if (Input.GetKey(KeyCode.LeftControl))
-        {
-            vehicleManager.raceCars[0].gameObject.GetComponent<AbilityCollision>().carAbilities.powerAmount -= 0.15f * Time.deltaTime;
-        }
     }
     void SetSliders()
     {
@@ -90,33 +84,9 @@ public class BoostBarScript : MonoBehaviour
             PositionUpdate currentVehicle = vehicleManager.ogRaceCars[i];
             AbilityCollision abilities = currentVehicle.gameObject.GetComponent<AbilityCollision>();
             boostBarSliders[i].value = abilities.carAbilities.powerAmount;
-            boostBarImages[i].color = barGradient.Evaluate(boostBarSliders[i].value);
+            //boostBarImages[i].color = barGradient.Evaluate(boostBarSliders[i].value);
+            boostBarImages[i].color = Color.Lerp(Color.yellow, Color.red, boostBarSliders[i].value);
         }
-    }
-
-    void DeterminePositions()
-    {
-        for (int i = 0; i < vehicleNum; i++)
-        {
-            PositionUpdate currentVehicle = vehicleManager.raceCars[i];
-            //currentVehicle.aheadOf = 0;
-
-            for (int j = 0; j < vehicleNum; j++)
-            {
-                if (vehicleManager.raceCars[i].gameObject.GetInstanceID() !=
-                   vehicleManager.raceCars[j].gameObject.GetInstanceID())
-                {
-                    if ((vehicleManager.raceCars[i].collidersHit > vehicleManager.raceCars[j].collidersHit) ||
-                        (vehicleManager.raceCars[i].laps > vehicleManager.raceCars[j].laps))
-                    {
-                       // currentVehicle.aheadOf++;
-                    }
-                }
-
-                //Debug.Log(vehicleManager.raceCars[i].gameObject.GetInstanceID());
-            }   
-        }
-        ApplyText();
     }
 
     private void ApplyText()
@@ -126,10 +96,9 @@ public class BoostBarScript : MonoBehaviour
             PositionUpdate currentVehicle = vehicleManager.ogRaceCars[i];
 
             // MANI'S UPDATE
-            // DeterminePositions() is not needed as RaceManager does all the switching
             int pos = currentVehicle.GetPosition();
 
-            switch (currentVehicle.GetPosition())
+            switch (pos)
             {
                 case 1:
                     boostBarText[i].text = "1ST";
@@ -144,34 +113,6 @@ public class BoostBarScript : MonoBehaviour
                     boostBarText[i].text = "4TH";
                     break;
             }
-            // END MANI'S UPDATE
-
-            //if (currentVehicle.aheadOf == vehicleNum - 1)
-            //{
-            //    //currentVehicle.ranking = PositionUpdate.VehiclePosition.FIRST;
-            //    boostBarText[i].text = "1ST";
-            //}
-
-            //else if (currentVehicle.aheadOf == vehicleNum - 2)
-            //{
-            //    boostBarText[i].text = "2ND";
-            //}
-
-            //else if (currentVehicle.aheadOf == vehicleNum - 3)
-            //{
-            //    boostBarText[i].text = "3RD";
-            //}
-
-            //else
-            //{
-            //     boostBarText[i].text = "4TH";
-            //}
-
-            //switch (currentVehicle.aheadOf)
-            //{ 
-            //    case (vehicleNum):
-            //        break;
-            //}
         }
     }
 }
