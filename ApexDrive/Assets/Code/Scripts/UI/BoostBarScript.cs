3d9359ea6d6a582cdfe88ab9275812d9aa5ae589
Zach Horton <Zachary2.Horton@live.uwe.ac.uk>
2021-06-06 07:17:04 +0100

    Further cleanup
    Further cleanup has been done as to remove comments used to indicate progress, and thus make the script more readable.

--- ApexDrive/Assets/Code/Scripts/UI/BoostBarScript.cs
+++ ApexDrive/Assets/Code/Scripts/UI/BoostBarScript.cs
@@ -15,17 +15,11 @@ public class BoostBarScript : MonoBehaviour
     private TextMeshProUGUI[] boostBarText;
     private Image[] boostBarImages;
 
-    //private Gradient barGradient; //gradient related properties
-    //private float colourTimer, alphaTimer;
-    //GradientColorKey[] colourKeys;
-    //GradientAlphaKey[] alphaKeys;
-
     void Start()
     {
         UICanvas = GetComponent<Canvas>();
         vehicleNum = vehicleManager.raceCars.Count;
         InitBarObjects();
-        //InitGradient();
     }
 
     private void InitBarObjects()
@@ -45,33 +39,6 @@ public class BoostBarScript : MonoBehaviour
         }
     }
 
-    //private void InitGradient()
-    //{
-    //    barGradient = new Gradient();
-    //    alphaKeys = new GradientAlphaKey[3];
-    //    alphaTimer = 0.0f;
-
-    //    for (int j = 0; j < alphaKeys.Length; j++)
-    //    {
-    //        alphaKeys[j].alpha = 1.0f;
-    //        alphaKeys[j].time = alphaTimer;
-    //        alphaTimer += 0.5f;
-    //    }
-
-    //    colourKeys = new GradientColorKey[3];
-    //    colourKeys[0].color = Color.yellow;
-    //    colourKeys[1].color = new Color(1.0f, 0.55f, 0.0f);
-    //    colourKeys[2].color = Color.red;
-    //    colourTimer = 0.0f;
-
-    //    for (int k = 0; k < colourKeys.Length; k++)
-    //    {
-    //        colourKeys[k].time = colourTimer;
-    //        colourTimer += 0.5f;
-    //    }
-    //    barGradient.SetKeys(colourKeys, alphaKeys);
-    //}
-
     void Update()
     {
         SetSliders();
@@ -84,7 +51,6 @@ public class BoostBarScript : MonoBehaviour
             PositionUpdate currentVehicle = vehicleManager.ogRaceCars[i];
             AbilityCollision abilities = currentVehicle.gameObject.GetComponent<AbilityCollision>();
             boostBarSliders[i].value = abilities.carAbilities.powerAmount;
-            //boostBarImages[i].color = barGradient.Evaluate(boostBarSliders[i].value);
             boostBarImages[i].color = Color.Lerp(Color.yellow, Color.red, boostBarSliders[i].value);
         }
     }
@@ -94,10 +60,8 @@ public class BoostBarScript : MonoBehaviour
         for (int i = 0; i < vehicleNum; i++)
         {
             PositionUpdate currentVehicle = vehicleManager.ogRaceCars[i];
-
             // MANI'S UPDATE
             int pos = currentVehicle.GetPosition();
-
             switch (pos)
             {
                 case 1:
