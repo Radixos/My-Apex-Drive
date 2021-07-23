6cc0ac4eab386388761fd5064c29f1d14303305a
AlecGamble <Alec.Gamble@live.co.uk>
2021-07-23 21:18:17 +0100

    Somewhat broken
    don't use this branch anymore - it is broken. I have exported the scene to a unity package and am going to try to import it into a working build. - Alec.

---
+++ ApexDrive/Assets/Code/Scripts/ColliderRemover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEditor;
+ 
+public static class RemoveCollider
+{
+    [MenuItem("Tools/Remove Colliders In Scene")]
+    static public void RemoveColliders()
+    {
+        Collider[] colliders = GameObject.FindObjectsOfType<Collider>();
+
+        foreach(Collider collider in colliders)
+        {
+            GameObject.DestroyImmediate(collider);
+        }
+    }
+}
